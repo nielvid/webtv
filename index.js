a5e1e5bcb7f9b0869764abe4592c8add04ac99a1
Nielvid <nielvidservices@gmail.com>
2020-11-09 15:56:37 +0100

    third commit to dev

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+var API_KEY = "79434ed98e8baff189e3363a55c40b9b"
+
 popularMovies()
+onAir()
 
 
 
@@ -30,38 +33,39 @@ function hideModal(){
     }
 
    
-        const API_KEY = "79434ed98e8baff189e3363a55c40b9b"
+       
 
+
+        /*function for banners ection*/
+        (function(){
+            
         axios.get("https://api.themoviedb.org/3/discover/movie?api_key=" + API_KEY).then((response) => {
             const info = response.data.results
 
-
-
+        let background = document.getElementById('text')
+        let span = document.querySelector('.sliders span')
+        let sliders = document.querySelector('.sliders h1')
+        let paragraph = document.querySelector('.sliders p')
 
             info.forEach(item => {
 
+             
 
-                /* let background = document.getElementById('text')*/
-                let span = document.querySelector('.sliders span')
-                let sliders = document.querySelector('.sliders h1')
-                let paragraph = document.querySelector('.sliders p')
-
-
-
-                /* background.style.background = "https://image.tmdb.org/t/p/w500" + item.poster_path*/
-                span.innerHTML = item.vote_average
+                background.style.background = `linear-gradient(90deg, rgba(0, 0, 0, 0.9) 0%, rgba(0, 0, 0, 0.9) 35%, rgba(255, 255, 255, 0.2) 100%) 50% / cover no-repeat, url('https://image.tmdb.org/t/p/w500${item.poster_path}')`
+                //background-size: cover;`
+                span.innerHTML= item.vote_average
                 sliders.innerHTML = item.title
                 paragraph.innerHTML = item.overview
-
-console.log(item.title)
+  
+/* if using only background image without the gradient*/
+//background.style.backgroundImage = `url('https://image.tmdb.org/t/p/w500${item.poster_path}')`*/
 
             })
 
-
-
+        
         }).catch(err => console.log(err))
 
-   
+    })()
        
     
 
@@ -70,7 +74,7 @@ console.log(item.title)
 
 /* function to fecth movies from tmdb*/
    function popularMovies(){
-    const API_KEY = "79434ed98e8baff189e3363a55c40b9b"
+    
   
     axios.get("https://api.themoviedb.org/3/movie/popular?api_key="+ API_KEY).then((response)=>{
         const movies = response.data.results
@@ -101,8 +105,50 @@ console.log(item.title)
            
         });
         
-        let container = document.querySelector('.movie-container')
-        container.innerHTML = data
+        
+        let container = document.querySelectorAll('.movie-container')
+        container[0].innerHTML = data
+    }).catch(err => console.log(err))
+}
+    
+
+/* function to fecth tn airing today from tmdb*/
+   function onAir(){
+    
+  
+    axios.get("https://api.themoviedb.org/3/tv/airing_today?api_key="+ API_KEY).then((response)=>{
+        const tv = response.data.results
+       
+  
+        let data =''
+   
+        tv.forEach(item => {
+           
+            
+         data += ` <!--movies box starts here-->
+            <div class="movies">
+                <div class="img-thumb">
+                    <a href="https://image.tmdb.org/t/p/w500${item.poster_path}" alt="movies to watch"><img src="https://image.tmdb.org/t/p/w500${item.poster_path}" alt="movies to watch" /> </a>
+                    <a class="movie-title" href="https://image.tmdb.org/t/p/w500${item.poster_path}">${item.name}</a>
+                 </div>
+                    <div class="details">
+                        <p>2020</p>
+                         <div class="reactions" >
+                            <i class="fas fa-eye"></i>
+                            <i class="fas fa-heart"></i>
+                        </div>
+                        <p><i class="fas fa-star"></i>${item.vote_count}</p>
+                    </div>
+            </div>
+            <!--movies box ends here-->
+            `
+            
+           
+        });
+        
+        
+        let container = document.querySelectorAll('.movie-container')
+        container[1].innerHTML = data
     }).catch(err => console.log(err))
 }
     

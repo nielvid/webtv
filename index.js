11665ee0e97456d4a31d07d4f812e4f14d115bb9
Nielvid <nielvidservices@gmail.com>
2020-11-09 01:41:55 +0100

    second commit to dev branch

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+popularMovies()
 
 
 
@@ -28,3 +28,81 @@ function hideModal(){
             modal.style.display = 'none'
         
     }
+
+   
+        const API_KEY = "79434ed98e8baff189e3363a55c40b9b"
+
+        axios.get("https://api.themoviedb.org/3/discover/movie?api_key=" + API_KEY).then((response) => {
+            const info = response.data.results
+
+
+
+
+            info.forEach(item => {
+
+
+                /* let background = document.getElementById('text')*/
+                let span = document.querySelector('.sliders span')
+                let sliders = document.querySelector('.sliders h1')
+                let paragraph = document.querySelector('.sliders p')
+
+
+
+                /* background.style.background = "https://image.tmdb.org/t/p/w500" + item.poster_path*/
+                span.innerHTML = item.vote_average
+                sliders.innerHTML = item.title
+                paragraph.innerHTML = item.overview
+
+console.log(item.title)
+
+            })
+
+
+
+        }).catch(err => console.log(err))
+
+   
+       
+    
+
+
+  
+
+/* function to fecth movies from tmdb*/
+   function popularMovies(){
+    const API_KEY = "79434ed98e8baff189e3363a55c40b9b"
+  
+    axios.get("https://api.themoviedb.org/3/movie/popular?api_key="+ API_KEY).then((response)=>{
+        const movies = response.data.results
+  
+        let data =''
+   
+        movies.forEach(item => {
+           
+            
+         data += ` <!--movies box starts here-->
+            <div class="movies">
+                <div class="img-thumb">
+                    <a href="https://image.tmdb.org/t/p/w500${item.poster_path}" alt="movies to watch"><img src="https://image.tmdb.org/t/p/w500${item.poster_path}" alt="movies to watch" /> </a>
+                    <a class="movie-title" href="https://image.tmdb.org/t/p/w500${item.poster_path}">${item.title}</a>
+                 </div>
+                    <div class="details">
+                        <p>2020</p>
+                         <div class="reactions" >
+                            <i class="fas fa-eye"></i>
+                            <i class="fas fa-heart"></i>
+                        </div>
+                        <p><i class="fas fa-star"></i>${item.vote_count}</p>
+                    </div>
+            </div>
+            <!--movies box ends here-->
+            `
+            
+           
+        });
+        
+        let container = document.querySelector('.movie-container')
+        container.innerHTML = data
+    }).catch(err => console.log(err))
+}
+    
